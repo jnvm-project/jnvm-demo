a450d638a791bb2cce048f9e09b80c88f63b3bd9
Anatole Lefort <anatole_lefort@telecom-sudparis.eu>
2022-05-10 22:00:23 +0200

    manual Account transformation

--- src/main/java/eu/telecomsudparis/jnvm/demo/Account.java
+++ src/main/java/eu/telecomsudparis/jnvm/demo/Account.java
@@ -1,33 +1,49 @@
 package eu.telecomsudparis.jnvm.demo;
 
-public class Account {
-    private final int id;
-    private long balance;
+import eu.telecomsudparis.jnvm.offheap.OffHeap;
+import eu.telecomsudparis.jnvm.offheap.OffHeapObjectHandle;
+
+public class Account extends OffHeapObjectHandle {
+
+    private static final long CLASS_ID = OffHeap.Klass.registerUserKlass(Account.class);
+    private static final long[] offsets = {0L, 4L};
+    private static final long SIZE = Integer.SIZE + Long.SIZE;
 
     Account(int id, long balance) {
-        this.id = id;
-        this.balance = balance;
+        super();
+        setIntegerField(offsets[0], id);
+        setLongField(offsets[1], balance);
     }
 
+    public Account(Void v, long offset) {
+        super(v, offset);
+    }
+
+    public long size() { return SIZE; }
+    public long classId() { return CLASS_ID; }
+    public void descend() { }
+
     protected int getId() {
-        return this.id;
+        return getIntegerField(offsets[0]);
     }
 
     protected long getBalance() {
-        return this.balance;
+        return getLongField(offsets[1]);
     }
 
     protected void setBalance(long balance) {
-        this.balance = balance;
+        setLongField(offsets[1], balance);
     }
 
     protected void incBalance(long delta) {
-        this.balance += delta;
+        setLongField(offsets[1], getLongField(offsets[1])+delta);
     }
 
     public void transferTo(Account dest, long amount) {
-        this.balance -= amount;
-        dest.balance += amount;
+        OffHeap.startRecording();
+        this.incBalance(-amount);
+        dest.incBalance(+amount);
+        OffHeap.stopRecording();
     }
 
 }
