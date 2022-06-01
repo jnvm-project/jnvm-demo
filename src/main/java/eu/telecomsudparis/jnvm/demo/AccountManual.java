12589ef5dfdb4c28fe8fd6a98deab94c3973aa77
Anatole Lefort <anatole_lefort@telecom-sudparis.eu>
2022-06-01 14:58:57 +0200

    make annotated entity default one and suffixes older one with « Manual »

---
+++ src/main/java/eu/telecomsudparis/jnvm/demo/AccountManual.java
@@ -0,0 +1,49 @@
+package eu.telecomsudparis.jnvm.demo;
+
+import eu.telecomsudparis.jnvm.offheap.OffHeap;
+import eu.telecomsudparis.jnvm.offheap.OffHeapObjectHandle;
+
+public class AccountManual extends OffHeapObjectHandle {
+
+    private static final long CLASS_ID = OffHeap.Klass.registerUserKlass(AccountManual.class);
+    private static final long[] offsets = {0L, 4L};
+    private static final long SIZE = Integer.SIZE + Long.SIZE;
+
+    AccountManual(int id, long balance) {
+        super();
+        setIntegerField(offsets[0], id);
+        setLongField(offsets[1], balance);
+    }
+
+    public AccountManual(Void v, long offset) {
+        super(v, offset);
+    }
+
+    public long size() { return SIZE; }
+    public long classId() { return CLASS_ID; }
+    public void descend() { }
+
+    protected int getId() {
+        return getIntegerField(offsets[0]);
+    }
+
+    protected long getBalance() {
+        return getLongField(offsets[1]);
+    }
+
+    protected void setBalance(long balance) {
+        setLongField(offsets[1], balance);
+    }
+
+    protected void incBalance(long delta) {
+        setLongField(offsets[1], getLongField(offsets[1])+delta);
+    }
+
+    public void transferTo(AccountManual dest, long amount) {
+        OffHeap.startRecording();
+        this.incBalance(-amount);
+        dest.incBalance(+amount);
+        OffHeap.stopRecording();
+    }
+
+}
